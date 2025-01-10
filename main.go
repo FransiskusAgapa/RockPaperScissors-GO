fafd77ac42d036c96b3729d4b7b6ba24d4200353
Fransiskus Agapa <agapaf@spu.edu>
2025-01-09 20:08:30 -0800

    added game todo task

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("test push")
+	/* TODO
+	* prompt use to enter their name
+	* prompt a set of action to take
+	* randomize an action for computer that'll act as opponent
+	* checking answer and announce the winner
+	* ask if user want to play again
+	* add scoreboard
+	 */
 	fmt.Println("\n==== Rock Paper Scissors ===")
 	fmt.Println("\n============================\n")
 }
+
+// GO Primitive Data Type
+// int, string, bool, float, rune
