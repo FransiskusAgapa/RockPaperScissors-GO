55a8fec4b1e957a2497de47fca9d5d98bad4609d
Fransiskus Agapa <agapaf@spu.edu>
2025-01-10 16:19:48 -0800

    added user prompt, validate input

--- main.go
+++ main.go
@@ -12,6 +12,21 @@ func main() {
 	* add scoreboard
 	 */
 	fmt.Println("\n==== Rock Paper Scissors ===")
+
+	// prompt action option
+	fmt.Print("\n> Pick an action:\n  1. Rock\n  2. Paper\n  3. Scissors\n\n> What's your move:")
+
+	// catch user input
+	var user_move int 
+
+	// validate ''user_move''
+	_, err := fmt.Scan(&user_move)
+	if err != nil {
+		fmt.Println("not a valid move")
+	} else {
+		fmt.Println("\n> Your move is ",user_move)
+	}
+
 	fmt.Println("\n============================\n")
 }
 
