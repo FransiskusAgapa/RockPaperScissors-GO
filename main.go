0ef820b4d6d4cecc52ba7d9bc467ccc6fc7e1a11
Fransiskus Agapa <agapaf@spu.edu>
2025-01-15 19:12:48 -0800

    finished basic game run

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 )
 
 func main() {
@@ -24,41 +26,48 @@ func main() {
 		3: "Scissors",
 	}
 
-	clearScreen()
+	//clearScreen()
 
 	// prompt action option
 	fmt.Print("\n> Pick an action:\n  1. Rock\n  2. Paper\n  3. Scissors\n\n> What's your move:")
 
 	// catch user input
-	var user_move int
+	var player_move int
 
 	// read error
 	var err error
 
 	// validate ''user_move''
-	_, err = fmt.Scan(&user_move)
+	_, err = fmt.Scan(&player_move)
 
-	for err != nil || user_move > len(actions) {
-		if err != nil{
-			fmt.Print("\n ! Cmon, you gotta pick a number\n\n> Try input your move again: ")
-			_, err = fmt.Scan(&user_move)
+	for err != nil || player_move > len(actions) {
+		if err != nil {
+			fmt.Print("\n ! C'mon, you gotta pick a number\n\n> Try input your move again: ")
+			_, err = fmt.Scan(&player_move)
 		}
-		
-		if err == nil && user_move > len(actions){
-			fmt.Print("\n ! Cmon, you gotta pick a number between 1 to 3\n\n> Try input your move again: ")
-			_, err = fmt.Scan(&user_move)
+
+		if err == nil && player_move > len(actions) {
+			fmt.Print("\n ! C'mon, you gotta pick a number between 1 to 3\n\n> Try input your move again: ")
+			_, err = fmt.Scan(&player_move)
 		}
-		
-		if err == nil && user_move <= len(actions) {
+
+		if err == nil && player_move <= len(actions) {
 			break
 		}
 	}
 
-	if user_move > len(actions){
-		fmt.Println("\n ! only choose between 1 to 3")
-	}
+	// generate random action for computer to simulate an opponent
+	rand.Seed(time.Now().UnixNano())
 
-	fmt.Println("\n> Your move is ", actions[user_move])
+	// generate random number
+	computer_move := rand.Intn(4)
+
+	fmt.Printf("\n> Your move is: %s\n", actions[player_move])
+	fmt.Printf("  Computer move is: %s\n", actions[computer_move])
+
+	// find out the winner
+	result := findWinner(actions[player_move], actions[computer_move])
+	fmt.Printf("\n> Result: %s", result)
 
 	fmt.Println("\n============================\n")
 }
@@ -76,4 +85,22 @@ func clearScreen() {
 	}
 	cmd.Stdout = os.Stdout
 	cmd.Run()
+}
+
+func findWinner(player_move string, computer_move string) string {
+	result := ""
+	// fmt.Printf("player: %s - opponent: %s", player_move, computer_move)
+	if player_move == computer_move {
+		result = "It is a Draw"
+	}
+	if player_move == "Rock" && computer_move == "Scissors" {
+		result = "You won!"
+	} else if player_move == "Paper" && computer_move == "Rock" {
+		result = "You won!"
+	} else if player_move == "Scissors" && computer_move == "Paper" {
+		result = "You won!"
+	} else {
+		result = "Computer won! "
+	}
+	return result
 }
